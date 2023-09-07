af30c50008a833b7c7acee6fa037ee560aef7a49
UnchartedWorld <nathangvjcollins@gmail.com>
2023-09-07 21:16:13 +0100

    feat(viewmodels): Adds default ViewModels files

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Job_Application_Recorder.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia! That, or you're lost. Oh well.";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Job_Application_Recorder.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
