d2c23e031f91ad3ab559ab84fc23d4258cc8972a
UnchartedWorld <nathangvjcollins@gmail.com>
2023-09-15 22:43:50 +0100

    feat(views): Adds basic concept for the landing page view
    This is not even close to complete, but this does serve as a general idea of what I want the landing page to look like.

---
+++ Views/LandingPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+
+namespace Job_Application_Recorder.Views;
+
+public partial class LandingPageView : UserControl
+{
+    public LandingPageView()
+    {
+        InitializeComponent();
+    }
+}
