1d45fc6d4498e57f45412b008151f4e50a18244a
UnchartedWorld <nathangvjcollins@gmail.com>
2023-09-19 21:47:43 +0100

    feat(views): Adds an intentionally non-functional DataGrid to ViewJob View
    This will eventually be connected to a backend, but I just wanted to make sure I had the files here.

---
+++ Views/ViewJobAppView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Job_Application_Recorder.Views;
+
+public partial class ViewJobAppView : UserControl
+{
+    public ViewJobAppView()
+    {
+        InitializeComponent();
+    }
+}
