9862530b7b6dd915de348ffe6e7ae95a25ebf509
UnchartedWorld <nathangvjcollins@gmail.com>
2023-09-21 15:58:14 +0100

    feat(model): Adds model for interview(s)
    This will be used in a List format, allowing for a nth number of
    interviews to be listed. This could prove useful later on, but I am
    concerned about how this will work with CSV files.

---
+++ Models/Interview.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Interview
+{
+    public DateTime DateOfInterview { get; set; }
+    public string Interviewees { get; set; }
+    public bool IsInterviewComplete { get; set; }
+    public string? InterviewNotes { get; set; }
+}
