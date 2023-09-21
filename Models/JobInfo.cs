15f22a44006c60b1684964b803bd68874bf0dd7e
UnchartedWorld <nathangvjcollins@gmail.com>
2023-09-21 15:50:51 +0100

    feat(model): Adds Model representing general job information
    As stated above, this provides a bunch of fields representing job information. Some of these are dependant on other models, which will be committed too.

---
+++ Models/JobInfo.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class JobInfo
+{
+    public string CompanyName { get; set; }
+    public string JobTitle { get; set; }
+    public Salary InputSalary { get; set; }
+    public string JobLink { get; set; }
+    public string JobLocation { get; set; }
+    public string JobFlexibility { get; set; }
+    public DateTime DateOfApplying { get; set; }
+    public JobOfferStatus OfferStatus { get; set; }
+
+    public enum JobOfferStatus
+    {
+        Yes,
+        No,
+        Unknown
+    }
+}
