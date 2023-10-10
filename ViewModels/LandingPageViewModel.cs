f58379a7becf30e3807534a871bdef78e713f01b
UnchartedWorld <nathangvjcollins@gmail.com>
2023-10-10 23:58:45 +0100

    feat(viewmodels): Adds select file property & function referring to DialogService
    This calls to the FileOpen function in DialogService, which should be expanded as time progresses.

--- ViewModels/LandingPageViewModel.cs
+++ ViewModels/LandingPageViewModel.cs
@@ -1,6 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Job_Application_Recorder.Services;
+using ReactiveUI;
+
 namespace Job_Application_Recorder.ViewModels;
 
 public class LandingPageViewModel : ViewModelBase
 {
+    private IEnumerable<string>? _selectedJSONFile;
+    public ICommand SelectFileCommand { get; } 
+
+    /// <summary>
+    /// Gets or sets a singular file.
+    /// </summary>
+    public IEnumerable<string>? SelectedJSONFile
+    {
+        get => _selectedJSONFile;
+        set => this.RaiseAndSetIfChanged(ref _selectedJSONFile, value);
+    }
+
+    public LandingPageViewModel()
+    {
+        SelectFileCommand = ReactiveCommand.CreateFromTask(SelectFileAsync);
+    }
+
+    public async Task SelectFileAsync()
+    {
+        SelectedJSONFile = await this.OpenFileDialogAsync("Open JSON File…");
+    }
     
 }
