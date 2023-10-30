0bfb7e0b2ffaa8cc52244097bd45579fabe9916e
UnchartedWorld <nathangvjcollins@gmail.com>
2023-10-30 21:19:03 +0000

    feat(models): Changes ID from int to Guid
    My reasoning is that, for a JSON file, this removes the chances of conflicts for the vast majority of cases, and I won't sort by ID anyway.

--- Models/JobApplication.cs
+++ Models/JobApplication.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 
 public class JobApplication
 {
-    public int ID { get; set; }
+    public Guid ID { get; set; }
     public JobInfo JobInformation { get; set; }
     public List<Interview> Interviews { get; set; }
     public JobNotes Notes { get; set; }
